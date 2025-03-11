3be62ccf6bfb7a3204e1d7a57c4803ac54de2223
uncledrew-sr <bonggutravel@gmail.com>
2025-03-11 10:01:18 +0900

    add sources

---
+++ echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+    <?php
+        echo "Cat and Rabbit";
+    ?>
+    </h3>
+    <?php
+        $img_cat = "source/02/cat.jpg";
+        echo "<img src = '$img_cat'>";
+        echo "<br>";
+        
+        $img_rabbit = "source/02/rabit.jpg";
+        echo "<img src = '$img_rabbit'>"
+    ?>
+</body>
+</html>
---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+	// 주석
+	/*
+	여러 줄 주석이다
+	이 색기야
+	*/
+	echo "Hello world"; // echo는 그대로 출력하라는 의미
+	print "Hello world"; // print는 echo와 동일하지만 echo를 더 자주 사용
+?>
